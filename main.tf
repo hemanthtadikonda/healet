eee97e0725b5af3e37c0b54b19861c751c205991
hemanth <hemanthtadikonda29@gmail.com>
2024-02-10 16:44:02 +0530

    kubernetes nodes

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
 resource "aws_instance" "leo" {
-  ami = var.ami
-  instance_type = var.instance_type
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [ var.sg_grp_id ]
-  tags = {
+  tags                   = {
     Name = var.project_name
   }
+  provisioner "local-exec" {
+    command = <<EOF
+  yum install ansible -y
+  sleep 20
+  ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/leo.git leo.yml
+  EOF
+
+  }
 }
+
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
@@ -14,3 +23,8 @@ resource "aws_route53_record" "www" {
   ttl     = 300
   records = [aws_instance.leo.public_ip]
 }
+
+
+
+
+
