1249e8f5a061795a002f21e8b965e18700452f72
hemanth <hemanthtadikonda29@gmail.com>
2024-02-10 15:57:55 +0530

    kubernetes nodes

--- main.tf
+++ main.tf
@@ -5,3 +5,19 @@ resource "aws_instance" "main" {
     Name = "leo"
   }
 }
+
+resource "aws_instance" "main" {
+  ami = "ami-0f3c7d07486cad139"
+  instance_type = "t2.small"
+  tags = {
+    Name = "harika"
+  }
+}
+
+resource "aws_instance" "main" {
+  ami = "ami-0f3c7d07486cad139"
+  instance_type = "t2.small"
+  tags = {
+    Name = "hemanth"
+  }
+}
